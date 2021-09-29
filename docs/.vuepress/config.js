a3cbc5c60649d21f80eec57d90fd0e6d07ae744e
goku <sharma.hunny1998@gmail.com>
2021-09-29 23:34:45 +0530

    Page split

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,7 @@ module.exports = {
         displayAllHeaders: true,
         nav: [
             {text: 'Home', link: '/'},
+            {text: 'Documentation', link: '/documentation/'},
             {text: 'Guide', link: '/guide/'},
 	    {text: 'Videos', link: 'https://www.youtube.com/watch?v=amAlwxCEgnI'},
         ],
